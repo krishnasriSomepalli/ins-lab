531aa079729f88cc133bfd790b9656f125948af9
Krishna Sri Somepalli <krishnasri.somepalli@outlook.com>
2019-04-29 04:04:02 +0530

    CryptoAES.java and CryptoDES.java simplified

--- CryptoDES.java
+++ CryptoDES.java
@@ -1,14 +1,9 @@
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.BadPaddingException;
-
-import java.security.NoSuchAlgorithmException;
-import java.security.InvalidKeyException;
 
 class CryptoDES {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
 
 		// check args
 		if(args.length != 1) {
@@ -16,45 +11,28 @@ class CryptoDES {
 			return;
 		}
 
-		try {
-			// create a DES key
-			KeyGenerator keyGen = KeyGenerator.getInstance("DES");
-			SecretKey key = keyGen.generateKey(); 
+		// create a DES key
+		KeyGenerator keyGen = KeyGenerator.getInstance("DES");
+		SecretKey key = keyGen.generateKey(); 
 
-			// create a Cipher instance by specifying the algorithm name, mode (optional), and padding scheme (optional).
-			Cipher des = Cipher.getInstance("DES/ECB/PKCS5Padding");
+		// create a Cipher instance by specifying the algorithm name, mode (optional), and padding scheme (optional).
+		Cipher des = Cipher.getInstance("DES/ECB/PKCS5Padding");
 
-			// convert plainText to byte[] format
-			String plainText = args[0];
-			byte[] plainBytes = plainText.getBytes();
+		// convert plainText to byte[] format
+		String plainText = args[0];
+		byte[] plainBytes = plainText.getBytes();
 
-			// encrypt plainBytes
-			des.init(Cipher.ENCRYPT_MODE, key);
-			byte[] encryptedBytes = des.doFinal(plainBytes);
-			String encryptedText = new String(encryptedBytes);
-			System.out.println("You entered: " + plainText);
-			System.out.println("Encrypted data (DES): " + encryptedText);
-		
-			// decrypt encryptedBytes
-			des.init(Cipher.DECRYPT_MODE, key);
-			byte[] decryptedBytes = des.doFinal(encryptedBytes);
-			String decryptedText = new String(decryptedBytes);
-			System.out.println("Decrypted data (DES): " + decryptedText);
-		}
-		catch(NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		catch(InvalidKeyException e) {
-			e.printStackTrace();
-		}
-		catch(IllegalBlockSizeException e) {
-			e.printStackTrace();
-		}
-		catch(BadPaddingException e) {
-			e.printStackTrace();
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
+		// encrypt plainBytes
+		des.init(Cipher.ENCRYPT_MODE, key);
+		byte[] encryptedBytes = des.doFinal(plainBytes);
+		String encryptedText = new String(encryptedBytes);
+		System.out.println("You entered: " + plainText);
+		System.out.println("Encrypted data (DES): " + encryptedText);
+	
+		// decrypt encryptedBytes
+		des.init(Cipher.DECRYPT_MODE, key);
+		byte[] decryptedBytes = des.doFinal(encryptedBytes);
+		String decryptedText = new String(decryptedBytes);
+		System.out.println("Decrypted data (DES): " + decryptedText);
 	}
 }
